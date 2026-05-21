9e7f3c6b02f9602090cdaad6bc38fad606815147
xllyiiiii <xianglylucky@gmail.com>
2026-05-21 08:38:02 +0800

    FIX: 修正migrate_db.py数据库路径

--- migrate_db.py
+++ migrate_db.py
@@ -2,7 +2,7 @@
 import sqlite3
 import os
 
-DB_PATH = os.path.join(os.path.dirname(__file__), '..', 'instance', 'app.db')
+DB_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'instance', 'app.db')
 
 if not os.path.exists(DB_PATH):
     print(f'DB not found at {DB_PATH}')
